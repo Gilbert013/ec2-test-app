3f31fb63fb94e1a6e4af0beef1ce7eb3785f2f2c
totojang18 <totojang18@naver.com>
2025-11-20 09:14:21 +0900

    ec2-test-app

---
+++ src/main/java/com/example/ec2_test_app/Ec2TestAppApplication.java
@@ -0,0 +1,21 @@
+package com.example.ec2_test_app;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class Ec2TestAppApplication {
+	@PostConstruct
+	public void init(){
+		//JVM 타임존을 한국 시간으로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2TestAppApplication.class, args);
+	}
+
+}
